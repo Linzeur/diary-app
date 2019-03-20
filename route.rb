e2a503892c484a853fec394a55b2c0684372bce1
paulotijero <paulotijero@gmail.com>
2019-03-20 17:39:00 -0500

    Add template daily

--- route.rb
+++ route.rb
@@ -1,8 +1,13 @@
 require "sinatra"
+require "sinatra/reloader"
+require "sinatra/content_for"
 require "erb"
 
 get "/" do
-  erb :index
+
+  erb :list, {:layout=> :layout}
+  erb :daily, {:layout=> :layout}
+  
 end
 
 set :port,8006

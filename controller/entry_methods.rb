fc6105275d59ec787c9d54125ad1eb04c109b536
Marieth <mperezzevallos@gmail.com>
2019-03-21 18:50:28 -0500

    Avance Marieth

--- controller/entry_methods.rb
+++ controller/entry_methods.rb
@@ -26,3 +26,41 @@ def update_data(id, entry)
     file.write data_file.to_json
   end
 end
+
+def update_delete_data(id, is_deleted)
+  data_file = read_data
+  if is_deleted==1
+    data_file[id]["is_deleted"] = is_deleted
+    data_file[id]["deleted_datetime"] = Time.now.strftime("%Y%m%d%H%M%S")
+  else
+    data_file[id]["is_deleted"] = is_deleted
+    data_file[id]["deleted_datetime"] = ""
+  end
+  File.open($path_data, "w+") do |file|
+    file.write data_file.to_json
+  end
+end
+
+def update_delete_data(id, is_deleted)
+  data_file = read_data
+  if is_deleted==1
+    data_file[id]["is_deleted"] = is_deleted
+    data_file[id]["deleted_datetime"] = Time.now.strftime("%Y%m%d%H%M%S")
+  else
+    data_file[id]["is_deleted"] = is_deleted
+    data_file[id]["deleted_datetime"] = ""
+  end
+  File.open($path_data, "w+") do |file|
+    file.write data_file.to_json
+  end
+end
+
+def update_highlight_data(id, highlight_flag)
+    data_file = read_data
+    data_file[id]["highlight"] = highlight_flag
+    File.open($path_data, "w+") do |file|
+    file.write data_file.to_json
+    end
+end
+
+
--- route.rb
+++ route.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/content_for"
 require "erb"
+require "./controller/entry_methods.rb"
 
 set :port,8000
 
@@ -9,4 +10,3 @@ get "/" do
   erb :list, {:layout=> :layout}
   erb :daily, {:layout=> :layout}
 end
-

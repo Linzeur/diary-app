80a3179eb9ac4dbe5e35fb69021885953348be17
paulotijero <paulotijero@gmail.com>
2019-03-21 18:05:49 -0500

    Add redirect route

--- route.rb
+++ route.rb
@@ -4,11 +4,35 @@ require "sinatra/content_for"
 require "erb"
 require "./controller/list_methods.rb"
 
-set :port,8000
+set :port,8001
 
 get "/" do
   @data = list_daily
-  erb :list, {:layout=> :layout}
-  erb :daily, {:layout=> :layout}
+  erb :list
+  erb :list_entry
+  erb :daily
 end
 
+get "/photo" do
+  @data = list_daily
+  erb :list
+  erb :add_photo
+  erb :daily
+end
+
+
+get "/add-entry" do
+  @data = list_daily
+  # TODO: Add new entry
+  erb :list
+  erb :list_entry
+end
+
+get "/delete" do
+  # TODO: Add delete logic
+  redirect "/"
+end
+
+get "/edit" do
+  # TODO: Add edit logic
+end

28882140bf39a71af2f9942ab7a7a44463369b4c
paulotijero <paulotijero@gmail.com>
2019-03-21 16:47:43 -0500

    Add call index

--- route.rb
+++ route.rb
@@ -2,10 +2,12 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/content_for"
 require "erb"
+require "./controller/list_methods.rb"
 
 set :port,8000
 
 get "/" do
+  @data = list_daily
   erb :list, {:layout=> :layout}
   erb :daily, {:layout=> :layout}
 end

8902820da33f133504bebf4f2d349985e787fda3
Brayan Ciudad <linzeur@gmail.com>
2019-03-22 00:33:21 -0500

    Fixes and Refactoring

--- controller/entry_methods.rb
+++ controller/entry_methods.rb
@@ -1,4 +1,5 @@
 require 'json'
+require "date"
 
 $path_data = "./data/entries.json"
 
@@ -9,20 +10,22 @@ def read_data
   data
 end
 
-def save_data(entry)
+def save_data(id, entry)
+  data[id] = entry
+  File.open($path_data, "w+") do |file|
+    file.write data.to_json
+  end
+end
+
+def add_data(entry)
   data_file = read_data
   identifier = Time.now.getutc.to_i
+  entry["datetime"] = DateTime.parse(entry["datetime"]).strftime("%Y%m%d%H%M%S")
   entry["id"] = identifier
-  data_file[identifier] = entry
-  File.open($path_data, "w+") do |file|
-    file.write data_file.to_json
-  end
+  save_data(identifier, entry)
 end
 
 def update_data(id, entry)
   data_file = read_data
-  data_file[id] = entry
-  File.open($path_data, "w+") do |file|
-    file.write data_file.to_json
-  end
+  save_data(id, entry)
 end

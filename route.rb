d31259adc1f4a9ebac85ab4381c5021b8b4190b0
Brayan Ciudad <linzeur@gmail.com>
2019-03-21 20:51:16 -0500

    Brayan's code

--- route.rb
+++ route.rb
@@ -2,11 +2,27 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/content_for"
 require "erb"
+require "./controller/entry_methods"
 
 set :port,8000
 
+#puts read_data
 get "/" do
-  erb :list, {:layout=> :layout}
-  erb :daily, {:layout=> :layout}
+  erb :list
+  erb :entry
+end
+
+post "/prueba" do
+  puts params.to_s
+  puts params[:archivo][:filename]
+  File.open(params[:archivo][:filename], "wb") do |file|
+    file.write params[:archivo][:tempfile].read
+  end
+  "Hola"
+end
+
+get "/photo" do
+  erb :list
+  erb :list_photos
 end
 
